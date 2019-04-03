a31047f130f2fc0e5f22099ff5e5a0fc8e1d0443
Monika Stokowska <30418684+Monika-Stokowska@users.noreply.github.com>
2019-04-03 15:41:22 +0200

    final commit

---
+++ src/pl/javaudacity/WordCount.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.io.File;
+
+
+public class WordCount {
+
+    public static void main(String[] args) throws Exception {
+        File file = new File("taleoftwocities.txt");
+        Scanner scanner = new Scanner(file);
+
+        int words = 0;
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+
+            words += line.split(" ").length;
+        }
+        System.out.println("The file contains "+words+" words.");
+
+
+    }
+}
+
